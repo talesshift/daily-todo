98ebf7961f55f1a619262819bb375e4dca4e8eed
tales shift <31869464+talesshift@users.noreply.github.com>
2022-06-04 21:42:23 -0300

    removed some pf the pre-made content that came with the template

--- src/main.jsx
+++ src/main.jsx
@@ -9,43 +9,10 @@ import { ThemeProvider } from "./useTheme";
 function App() {
   return (
     <ThemeProvider>
-      <Column align="center" justify="center" css={{ height: "100%" }}>
-        <ThemeToggle css={{ position: "absolute", top: "$20", right: "$20" }} />
-        <Hero>Welcome!</Hero>
-        <p>
-          Edit <Code>src/main.jsx</Code> and save to reload.
-        </p>
-        <p>
-          Learn{" "}
-          <Link href="https://vitejs.dev" newtab>
-            Vite
-          </Link>{" "}
-          <Link href="https://eslint.org" newtab>
-            ESLint
-          </Link>{" "}
-          <Link href="https://reactjs.org" newtab>
-            React
-          </Link>{" "}
-          <Link href="https://stitches.dev" newtab>
-            Stitches
-          </Link>{" "}
-          <Link href="https://www.radix-ui.com" newtab>
-            Radix (Primitives, Colors, Icons)
-          </Link>
-        </p>
-      </Column>
+      
     </ThemeProvider>
   );
 }
-
-const Hero = styled("header", {
-  font: "$hero",
-  paddingBottom: "$20",
-});
-
-const Code = styled("code", {
-  fontFamily: "$mono",
-});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -53,3 +20,43 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+//from the theme this was inside the <ThemeProvider/>
+
+/* <Column align="center" justify="center" css={{ height: "100%" }}>
+  <ThemeToggle css={{ position: "absolute", top: "$20", right: "$20" }} />
+  <Hero>Welcome!</Hero>
+  <p>
+    Edit <Code>src/main.jsx</Code> and save to reload.
+  </p>
+  <p>
+    Learn{" "}
+    <Link href="https://vitejs.dev" newtab>
+      Vite
+    </Link>{" "}
+    <Link href="https://eslint.org" newtab>
+      ESLint
+    </Link>{" "}
+    <Link href="https://reactjs.org" newtab>
+      React
+    </Link>{" "}
+    <Link href="https://stitches.dev" newtab>
+      Stitches
+    </Link>{" "}
+    <Link href="https://www.radix-ui.com" newtab>
+      Radix (Primitives, Colors, Icons)
+    </Link>
+  </p>
+</Column> */
+
+//this was defined after the function App(){} was defined
+
+/* const Hero = styled("header", {
+  font: "$hero",
+  paddingBottom: "$20",
+});
+
+const Code = styled("code", {
+  fontFamily: "$mono",
+});*/
+
